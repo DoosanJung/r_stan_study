c9bf759303540d7c39aac6505901cb5c43c754b7
DoosanJung <dootaa00@gmail.com>
2020-02-26 21:16:03 -0700

    apply lintr result

--- linear_regression_example/run_multiple_linear_regression_example.R
+++ linear_regression_example/run_multiple_linear_regression_example.R
@@ -16,7 +16,7 @@ K <- dim(x)[2]
 
 fit <- stan(
   file = "multiple_linear_regression.stan",  # Stan program
-  data = list(N=N, K=K, x=x, y=y),    # named list of data
+  data = list(N = N, K = K, x = x, y = y),    # named list of data
   chains = 4,             # number of Markov chains
   warmup = 1000,          # number of warmup iterations per chain
   iter = 2000,            # total number of iterations per chain
@@ -24,12 +24,12 @@ fit <- stan(
   refresh = 0             # no progress shown
 )
 
-print(fit, pars=c("alpha", "beta", "lp__"), probs=c(.1,.5,.9))
+print(fit, pars = c("alpha", "beta", "lp__"), probs = c(.1, .5, .9))
 plot(fit)
-traceplot(fit, pars=c("alpha", "beta"), inc_warmup = TRUE, nrow = 2)
-pairs(fit, pars=c("alpha", "beta", "lp__"))
+traceplot(fit, pars = c("alpha", "beta"), inc_warmup = TRUE, nrow = 2)
+pairs(fit, pars = c("alpha", "beta", "lp__"))
 
 
 # using R's lm to fit the linear model
-result <- lm(mpg ~ disp + hp + wt, data=mtcars)
+result <- lm(mpg ~ disp + hp + wt, data = mtcars)
 print(summary(result))
--- linear_regression_example/run_simple_linear_regression_example.R
+++ linear_regression_example/run_simple_linear_regression_example.R
@@ -14,7 +14,7 @@ y <- cars$dist
 
 fit <- stan(
   file = "simple_linear_regression.stan",  # Stan program
-  data = list(N=N, x=x, y=y),    # named list of data
+  data = list(N = N, x = x, y = y),    # named list of data
   chains = 4,             # number of Markov chains
   warmup = 1000,          # number of warmup iterations per chain
   iter = 2000,            # total number of iterations per chain
@@ -22,10 +22,10 @@ fit <- stan(
   refresh = 0             # no progress shown
 )
 
-print(fit, pars=c("alpha", "beta", "lp__"), probs=c(.1,.5,.9))
+print(fit, pars = c("alpha", "beta", "lp__"), probs = c(.1, .5, .9))
 plot(fit)
-traceplot(fit, pars=c("alpha", "beta"), inc_warmup = TRUE, nrow = 2)
-pairs(fit, pars=c("alpha", "beta", "lp__"))
+traceplot(fit, pars = c("alpha", "beta"), inc_warmup = TRUE, nrow = 2)
+pairs(fit, pars = c("alpha", "beta", "lp__"))
 
 
 # using R's lm to fit the linear model
